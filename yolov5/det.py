a13658d4647d1d519d301cb04377bf0fee8c1daa
Parth Bansal <33196466+parthbansal05@users.noreply.github.com>
2022-06-08 20:32:48 +0530

    .

--- yolov5/det.py
+++ yolov5/det.py
@@ -71,3 +71,59 @@ def detect(
                 # LOGGER.info(str(xywh) + '\n')
                 # LOGGER.info(str(label) + '\n')
     return lables, cords
+
+
+
+@torch.no_grad()
+def detect_preloaded(
+    weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
+        source='',  # file/dir/URL/glob, 0 for webcam
+        data=ROOT / 'data/coco128.yaml',  # dataset.yaml path
+):
+
+    conf_thres = 0.25
+    iou_thres = 0.45
+    max_det = 1000
+    device = ''
+    classes = None
+    agnostic_nms = False
+
+    device = select_device(device)
+    model = DetectMultiBackend(weights, device=device, dnn=False, data=data, fp16=False)
+    stride, names, pt = model.stride, model.names, model.pt
+    
+    # im0 = cv2.imread(source)  # BGR
+
+    im = letterbox(source, 640, stride, pt)[0]
+
+    # Convert
+    im = im.transpose((2, 0, 1))[::-1]  # HWC to CHW, BGR to RGB
+    im = np.ascontiguousarray(im)
+
+    im = torch.from_numpy(im).to(device)
+    im = im.half() if model.fp16 else im.float()  # uint8 to fp16/32
+    im /= 255  # 0 - 255 to 0.0 - 1.0
+    if len(im.shape) == 3:
+        im = im[None]  # expand for batch dim
+    gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+
+    # Inference
+    pred = model(im, augment=False)
+    pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
+    
+    lables=[]
+    cords=[]
+
+    for i, det in enumerate(pred):  # per image
+        if len(det):
+            # Rescale boxes from img_size to im0 size
+            det[:, :4] = scale_coords(im.shape[2:], det[:, :4], im0.shape).round()
+            for *xyxy, conf, cls in reversed(det):
+                xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                c = int(cls)
+                label = f'{names[c]} {conf:.2f}'
+                lables.append(xywh)
+                cords.append(label)
+                # LOGGER.info(str(xywh) + '\n')
+                # LOGGER.info(str(label) + '\n')
+    return lables, cords
